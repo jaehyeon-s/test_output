30c26f6019fab0af3955d81b8d842f44993e4bd5
Jae <83636246+jaehyeon-s@users.noreply.github.com>
2022-05-04 10:49:21 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptool.setup(
+  include_package_data = True,
+  name = 'test_output2143527',
+  version = '1.0.0',
+  description = 'oss-dev test_output',
+  author = 'jaehyeonseol',
+  author_email = 'qkstm03@naver.com'
+  url = 'https://github.com/jaehyeon-s/test_output',
+  install_requires = ['pytest'],
+  long_description = 'oss-dev output python module',
+  long_description_content_type = 'text/markdown',
+  classifiers = [
+     "Programming Language :: Python :: 3",
+     "Operating System :: OS Independent",
+  ],
+)
